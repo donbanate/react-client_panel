eb44cc8dcc299fa47cb0af88afd4cf5dee858ac2
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-09 03:34:48 +0800

    Create notify settings

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 // reducers
+import notifyReducer from "./reducers/notifyReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzHtZmVS_YE_QkkXvmr1BF_F6PznOHCGQ",
@@ -35,7 +36,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 // Create initial state
